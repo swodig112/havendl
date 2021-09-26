5eabcd78d5c5e195249031e6c1277f09d75bf1f4
swodig 112 <swodig@gmail.com>
2021-09-26 08:21:27 +0330

    Rename variable. Nothing to see here.

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import json
 import pyexiv2
 
 
-def download(code, api_key=None):
+def download(havenid, api_key=None):
     if api_key is None:
-        url = "https://wallhaven.cc/api/v1/w/{0}".format(code)
+        url = "https://wallhaven.cc/api/v1/w/{0}".format(havenid)
     else:
-        url = "https://wallhaven.cc/api/v1/w/{0}?apikey={1}".format(code, api_key)
+        url = "https://wallhaven.cc/api/v1/w/{0}?apikey={1}".format(havenid, api_key)
     data = json.loads(requests.get(url).content)["data"]
     path = data["path"]
     frmt = path[::-1][:path[::-1].find('.')][::-1]
-    filepath = "wallhaven-{0}.{1}".format(code, frmt)
+    filepath = "wallhaven-{0}.{1}".format(havenid, frmt)
 
     img = open(filepath, 'wb')
     img.write(requests.get(path).content)
